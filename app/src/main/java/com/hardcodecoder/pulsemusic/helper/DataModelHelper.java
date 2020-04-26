a1d05fb73ac859dbe67a8b4a13c60ba79d5dfefb
HardcodeCoder <hardcodecoder@gmail.com>
2020-04-26 19:34:07 +0530

    added helper class for restoring saved playlist

---
+++ app/src/main/java/com/hardcodecoder/pulsemusic/helper/DataModelHelper.java
@@ -0,0 +1,20 @@
+package com.hardcodecoder.pulsemusic.helper;
+
+import com.hardcodecoder.pulsemusic.model.MusicModel;
+import com.hardcodecoder.pulsemusic.singleton.TrackManager;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class DataModelHelper {
+
+    public static List<MusicModel> getModelFromTitle(List<String> titles) {
+        List<MusicModel> modelList = new ArrayList<>();
+        Map<String, MusicModel> modelMap = TrackManager.getInstance().getModelMap();
+        for (String name : titles) {
+            modelList.add(modelMap.get(name));
+        }
+        return modelList;
+    }
+}
