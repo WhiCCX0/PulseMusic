83afe1cab62bda70256f52c0c0aefa5c0a05bb8e
HardcodeCoder <hardcodecoder@gmail.com>
2020-04-28 15:56:49 +0530

    refactor

--- app/src/main/java/com/hardcodecoder/pulsemusic/themes/ThemeManager.java
+++ app/src/main/java/com/hardcodecoder/pulsemusic/themes/ThemeManager.java
@@ -4,49 +4,37 @@ import android.content.Context;
 
 import androidx.annotation.StyleRes;
 
-import com.hardcodecoder.pulsemusic.helper.HomeWalliProvider;
-import com.hardcodecoder.pulsemusic.helper.HomeWalliProvider.Day;
 import com.hardcodecoder.pulsemusic.utils.AppSettings;
+import com.hardcodecoder.pulsemusic.utils.DayTimeUtils;
+import com.hardcodecoder.pulsemusic.utils.DayTimeUtils.DayTime;
 
 public class ThemeManager {
 
     private static boolean mDarkMode = false;
-    private static boolean mAuto = false;
     private static int mThemeId;
     private static int mAccentId;
 
-    public static void init(Context context){
-        mAuto = AppSettings.isAutoThemeEnabled(context);
-        if (mAuto) {
-            Day day = HomeWalliProvider.getTimeOfDay();
-            mDarkMode = (day == Day.EVENING || day == Day.NIGHT);
-        } else mDarkMode = AppSettings.isDarkModeEnabled(context);
+    public static void init(Context context) {
+        boolean auto = AppSettings.isAutoThemeEnabled(context);
+        if (auto) mDarkMode = (DayTimeUtils.getTimeOfDay() == DayTime.NIGHT);
+        else mDarkMode = AppSettings.isDarkModeEnabled(context);
 
-        if(mDarkMode) mThemeId = AppSettings.getSelectedDarkTheme(context);
-        else mThemeId = ThemeStore.LIGHT_THEME_1;//AppSettings.getSelectedLightTheme(context);
+        if (mDarkMode) mThemeId = AppSettings.getSelectedDarkTheme(context);
+        else mThemeId = ThemeStore.LIGHT_THEME_1;
         mAccentId = AppSettings.getSelectedAccentColor(context);
-    }
-
-    public static boolean isAutoThemeEnabled() {
-        return mAuto;
     }
 
     public static boolean isDarkModeEnabled() {
         return mDarkMode;
     }
 
-    public static void enableDarkMode(Context context, boolean enable){
+    public static void enableDarkMode(Context context, boolean enable) {
         AppSettings.enableDarkMode(context, enable);
     }
 
     public static void enableAutoTheme(Context context, boolean enable) {
         AppSettings.enableAutoTheme(context, enable);
     }
-
-    /*public static void setSelectedLightTheme(Context context, int id) {
-        mThemeId = id;
-        AppSettings.saveSelectedLightTheme(context, mThemeId);
-    }*/
 
     public static void setSelectedDarkTheme(Context context, int id) {
         mThemeId = id;
@@ -64,5 +52,7 @@ public class ThemeManager {
     }
 
     @StyleRes
-    public static int getAccentToApply() { return ThemeStore.getAccentById(mDarkMode, mAccentId); }
+    public static int getAccentToApply() {
+        return ThemeStore.getAccentById(mDarkMode, mAccentId);
+    }
 }
