1db69bdc1d2032d1d5a750d1e0ca41b3b9445627
HardcodeCoder <hardcodecoder@gmail.com>
2020-05-07 21:09:28 +0530

    minimal ui

--- app/src/main/java/com/hardcodecoder/pulsemusic/dialog/HomeBottomSheetFragment.java
+++ app/src/main/java/com/hardcodecoder/pulsemusic/dialog/HomeBottomSheetFragment.java
@@ -1,90 +1,81 @@
-package com.hardcodecoder.pulsemusic.ui;
+package com.hardcodecoder.pulsemusic.dialog;
 
 import android.app.Activity;
-import android.app.Dialog;
 import android.content.Intent;
-import android.net.Uri;
+import android.media.audiofx.AudioEffect;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.textview.MaterialTextView;
 import com.hardcodecoder.pulsemusic.GlideApp;
 import com.hardcodecoder.pulsemusic.R;
-import com.hardcodecoder.pulsemusic.activities.AppInfo;
 import com.hardcodecoder.pulsemusic.activities.SettingsActivity;
+import com.hardcodecoder.pulsemusic.ui.CustomBottomSheet;
 import com.hardcodecoder.pulsemusic.utils.UserInfo;
 
-import java.util.Objects;
 
-public class HomeBottomSheetFragment extends BottomSheetDialogFragment {
+public class HomeBottomSheetFragment extends RoundedBottomSheetDialogFragment {
 
-    static final String TAG = "HomeBottomSheetFragment";
+    public static final String TAG = "HomeBottomSheetFragment";
+    private static final int REQUEST_CODE_OPEN_EQUALIZER = 599;
     private static final int PICK_AVATAR = 1500;
-    private ImageView profilePic;
-    private TextView mUserName;
+    private ImageView mUserPic;
+    private MaterialTextView mUserName;
 
-    static HomeBottomSheetFragment newInstance() {
+    public static HomeBottomSheetFragment newInstance() {
         return new HomeBottomSheetFragment();
     }
 
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return new BottomSheetDialog(Objects.requireNonNull(getContext()), R.style.BottomSheetDialog);
-    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_bottom_sheet_dialog, container, false);
+        return inflater.inflate(R.layout.boottom_sheet_home_dialog, container, false);
     }
-
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        mUserName = view.findViewById(R.id.user_name);
-
+        mUserPic = view.findViewById(R.id.drawer_user_logo);
+        loadProfilePic();
+        mUserName = view.findViewById(R.id.drawer_user_name);
         if (null != getContext()) updateUserName(UserInfo.getUserName(getContext()));
 
-        profilePic = view.findViewById(R.id.user_profile);
-        loadProfilePic();
-
+        mUserPic.setOnClickListener(v -> pickPhoto());
         mUserName.setOnClickListener(v1 -> addUserName());
-
-        profilePic.setOnClickListener(v1 -> pickPhoto());
-
-        view.findViewById(R.id.check_source_code).setOnClickListener(v1 -> openLink(getString(R.string.source_code_link)));
-
-        view.findViewById(R.id.git_profile).setOnClickListener(v1 -> openLink(getString(R.string.github_link)));
-
-        view.findViewById(R.id.app_info).setOnClickListener(v1 -> startActivity(new Intent(getContext(), AppInfo.class)));
-
-        view.findViewById(R.id.settings).setOnClickListener(v1 -> {
+        view.findViewById(R.id.drawer_option_settings).setOnClickListener(v -> {
             startActivity(new Intent(getContext(), SettingsActivity.class));
             if (isVisible()) dismiss();
+        });
+        view.findViewById(R.id.drawer_option_equalizer).setOnClickListener(v -> {
+            final Intent intent = new Intent(AudioEffect.ACTION_DISPLAY_AUDIO_EFFECT_CONTROL_PANEL);
+            if (null != getContext()) {
+                if ((intent.resolveActivity(getContext().getPackageManager()) != null))
+                    startActivityForResult(intent, REQUEST_CODE_OPEN_EQUALIZER);
+                else
+                    Toast.makeText(getContext(), getString(R.string.equalizer_error), Toast.LENGTH_SHORT).show();
+            }
         });
     }
 
     private void loadProfilePic() {
         GlideApp.with(this)
-                .load(UserInfo.getUserProfilePic(profilePic.getContext()))
+                .load(UserInfo.getUserProfilePic(mUserPic.getContext()))
                 .error(R.drawable.def_avatar)
                 .circleCrop()
-                .into(profilePic);
+                .into(mUserPic);
     }
 
     private void pickPhoto() {
@@ -100,23 +91,17 @@ public class HomeBottomSheetFragment extends BottomSheetDialogFragment {
         startActivityForResult(chooserIntent, PICK_AVATAR);
     }
 
-    private void openLink(String url) {
-        Intent i = new Intent(Intent.ACTION_VIEW);
-        i.setData(Uri.parse(url));
-        startActivity(i);
-    }
-
     private void updateUserName(String title) {
         mUserName.setText(title);
     }
 
     private void addUserName() {
-        View layout = View.inflate(getContext(), R.layout.dialog_create_playlist, null);
+        View layout = View.inflate(getContext(), R.layout.bottom_dialog_edit_text, null);
         BottomSheetDialog sheetDialog = new CustomBottomSheet(layout.getContext());
         sheetDialog.setContentView(layout);
         sheetDialog.show();
 
-        ((TextView) layout.findViewById(R.id.header)).setText(getResources().getString(R.string.enter_name));
+        ((MaterialTextView) layout.findViewById(R.id.header)).setText(getResources().getString(R.string.enter_name));
         ((TextInputLayout) layout.findViewById(R.id.edit_text_container)).setHint(getResources().getString(R.string.enter_name));
         TextInputEditText et = layout.findViewById(R.id.text_input_field);
 
