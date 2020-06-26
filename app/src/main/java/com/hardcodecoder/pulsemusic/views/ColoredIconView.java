59b6e8eb43d8586ba41b1294dc4197bd5f369915
HardcodeCoder <hardcodecoder@gmail.com>
2020-06-26 17:42:49 +0530

    use desaturated color if preference is true
    Earlier colors were desaturated based on theme, now desaturated color only when option is turned on to use desaturated colors.

--- app/src/main/java/com/hardcodecoder/pulsemusic/views/ColoredIconView.java
+++ app/src/main/java/com/hardcodecoder/pulsemusic/views/ColoredIconView.java
@@ -11,7 +11,7 @@ import androidx.appcompat.widget.AppCompatImageView;
 
 import com.hardcodecoder.pulsemusic.R;
 import com.hardcodecoder.pulsemusic.themes.ColorUtil;
-import com.hardcodecoder.pulsemusic.themes.ThemeManagerUtils;
+import com.hardcodecoder.pulsemusic.utils.AppSettings;
 
 public class ColoredIconView extends AppCompatImageView {
 
@@ -33,18 +33,18 @@ public class ColoredIconView extends AppCompatImageView {
     private void initialize(Context context, @Nullable AttributeSet attributeSet) {
         TypedArray typedArray = context.obtainStyledAttributes(attributeSet, R.styleable.ColoredIconView);
         setBackground(context.getDrawable(R.drawable.plain_circle));
-        int paddingPixels = context.getResources().getDimensionPixelSize(R.dimen.icon_padding);
+        int paddingPixels = context.getResources().getDimensionPixelSize(R.dimen.colored_icon_padding);
         setPadding(paddingPixels, paddingPixels, paddingPixels, paddingPixels);
         setImageResource(typedArray.getResourceId(R.styleable.ColoredIconView_icon, R.drawable.def_colored_icon_view_icon));
 
         int iconColor = typedArray.getColor(R.styleable.ColoredIconView_iconColor, Color.BLUE);
         int iconBackgroundColor = typedArray.getColor(R.styleable.ColoredIconView_iconBackgroundColor, iconColor);
 
-        boolean desaturated = ThemeManagerUtils.isDarkModeEnabled() && typedArray.getBoolean(R.styleable.ColoredIconView_desaturatedColorInDarkMode, true);
+        boolean desaturated = AppSettings.getAccentDesaturatedColor(context) && typedArray.getBoolean(R.styleable.ColoredIconView_desaturatedColorInDarkMode, true);
 
         if (desaturated) {
             iconColor = context.getResources().getColor(R.color.darkColorBackground);
-            iconBackgroundColor = ColorUtil.mixColors(iconBackgroundColor, Color.WHITE, 0.4f);
+            iconBackgroundColor = ColorUtil.mixColors(iconBackgroundColor, Color.WHITE, 0.45f);
         } else
             iconBackgroundColor = ColorUtil.changeColorAlphaTo20(iconBackgroundColor);
 
--- app/src/main/java/com/hardcodecoder/pulsemusic/views/SettingsCategoryItemView.java
+++ app/src/main/java/com/hardcodecoder/pulsemusic/views/SettingsCategoryItemView.java
@@ -16,7 +16,7 @@ import androidx.annotation.Nullable;
 import com.google.android.material.textview.MaterialTextView;
 import com.hardcodecoder.pulsemusic.R;
 import com.hardcodecoder.pulsemusic.themes.ColorUtil;
-import com.hardcodecoder.pulsemusic.themes.ThemeManagerUtils;
+import com.hardcodecoder.pulsemusic.utils.AppSettings;
 
 public class SettingsCategoryItemView extends FrameLayout {
 
@@ -55,7 +55,7 @@ public class SettingsCategoryItemView extends FrameLayout {
             int iconColor = typedArray.getColor(R.styleable.SettingsCategoryItemView_settingItemIconColor, Color.BLUE);
             int iconBackgroundColor = typedArray.getColor(R.styleable.SettingsCategoryItemView_settingItemIconBackgroundColor, iconColor);
 
-            boolean desaturated = ThemeManagerUtils.isDarkModeEnabled()
+            boolean desaturated = AppSettings.getAccentDesaturatedColor(context)
                     && typedArray.getBoolean(R.styleable.SettingsCategoryItemView_settingItemDesaturatedColorsInDarkMode, true);
 
             if (desaturated) {
